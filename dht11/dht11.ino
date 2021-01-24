178cd14156f5ca5236614a8ccd5d7889e1e3ee8d
Masaya Nakamura <mashabow@shiromoji.net>
2021-01-24 16:27:30 +0900

    feat: connect to a WiFi access point

--- dht11/dht11.ino
+++ dht11/dht11.ino
@@ -1,13 +1,36 @@
+#include <ESP8266WiFi.h>
 #include "DHT.h"
 
 #define DHTPIN D6     // D6 ピンからセンサーの値を得る
 #define DHTTYPE DHT11 // DHT 11 を使う
 
+const char *ssid = "ssid";
+const char *password = "password";
+
+WiFiClient client;
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
   Serial.begin(115200);
+
+  // WiFi に接続する
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  WiFi.printDiag(Serial);
+
   dht.begin();
 }
 
