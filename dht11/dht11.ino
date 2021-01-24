8c1ca9df5758f29a2524c8cb75d3fe433e773e3e
Masaya Nakamura <mashabow@shiromoji.net>
2021-01-24 14:07:59 +0900

    feat: get humidity and temperature with DHT11

---
+++ dht11/dht11.ino
@@ -0,0 +1,21 @@
+#include "DHT.h"
+
+#define DHTPIN D6     // D6 ピンからセンサーの値を得る
+#define DHTTYPE DHT11 // DHT 11 を使う
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop()
+{
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  Serial.println("Temperature: " + String(t, 1) + "°C\tHumidity: " + String(h, 0) + "%");
+
+  delay(2000);
+}
