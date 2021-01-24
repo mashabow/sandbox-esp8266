f9d02d4dc213073ec8efcb62ed0a3836f114729b
Masaya Nakamura <mashabow@shiromoji.net>
2021-01-24 23:06:43 +0900

    feat: print the temperature and humidity on LCD

--- dht11/dht11.ino
+++ dht11/dht11.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include "LiquidCrystal_I2C.h"
 #include "DHT.h"
 
 #define DHTPIN D6     // D6 ピンからセンサーの値を得る
@@ -8,6 +9,7 @@ const char *ssid = "ssid";
 const char *password = "password";
 
 WiFiClient client;
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
@@ -31,6 +33,11 @@ void setup()
 
   WiFi.printDiag(Serial);
 
+  // LCD を使う
+
+  lcd.init();
+  lcd.backlight();
+
   dht.begin();
 }
 
@@ -39,6 +46,11 @@ void loop()
   float t = dht.readTemperature();
   float h = dht.readHumidity();
   Serial.println("Temperature: " + String(t, 1) + "°C\tHumidity: " + String(h, 0) + "%");
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp:     " + String(t, 1) + char(0xdf) + "C");
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity:    " + String(h, 0) + "%");
 
   delay(2000);
 }
